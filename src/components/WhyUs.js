0c5b003882dd6b4ee01f0ed37ccc0ca04786798b
Александр Морозов <frostmoroz83@yandex.ru>
2018-11-12 09:52:48 +0400

    убрал ошибки

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -8,40 +8,40 @@ class WhyUs extends Component {
   
   render() {
     return (
-    <div id="why-us" class="section">
+    <div id="why-us" className="section">
 
-    <div class="container">
+    <div className="container">
 
-      <div class="row">
-        <div class="section-header text-center">
+      <div className="row">
+        <div className="section-header text-center">
           <h2>Why Edusite</h2>
-          <p class="lead">Libris vivendo eloquentiam ex ius, nec id splendide abhorreant.</p>
+          <p className="lead">Libris vivendo eloquentiam ex ius, nec id splendide abhorreant.</p>
         </div>
 
-        <div class="col-md-4">
-          <div class="feature">
-            <i class="feature-icon fa fa-flask"></i>
-            <div class="feature-content">
+        <div className="col-md-4">
+          <div className="feature">
+            <i className="feature-icon fa fa-flask"></i>
+            <div className="feature-content">
               <h4>Online Courses</h4>
               <p>Ceteros fuisset mei no, soleat epicurei adipiscing ne vis. Et his suas veniam nominati.</p>
             </div>
           </div>
         </div>
 
-        <div class="col-md-4">
-          <div class="feature">
-            <i class="feature-icon fa fa-users"></i>
-            <div class="feature-content">
+        <div className="col-md-4">
+          <div className="feature">
+            <i className="feature-icon fa fa-users"></i>
+            <div className="feature-content">
               <h4>Expert Teachers</h4>
               <p>Ceteros fuisset mei no, soleat epicurei adipiscing ne vis. Et his suas veniam nominati.</p>
             </div>
           </div>
         </div>
 
-        <div class="col-md-4">
-          <div class="feature">
-            <i class="feature-icon fa fa-comments"></i>
-            <div class="feature-content">
+        <div className="col-md-4">
+          <div className="feature">
+            <i className="feature-icon fa fa-comments"></i>
+            <div className="feature-content">
               <h4>Community</h4>
               <p>Ceteros fuisset mei no, soleat epicurei adipiscing ne vis. Et his suas veniam nominati.</p>
             </div>
